e0dc01f11ca6e9f0fb37db0a039ba9976831ec89
David Gnamuš <38346533+dgnamus@users.noreply.github.com>
2022-06-02 07:21:44 +0200

    Python exercises

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# I announce I will count chickens
+print("I will now count my chickens:")
+
+# I count hens and roosters
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# I announce I will count the eggs
+print("Now I will count the eggs:")
+
+# ...and proceed to do so
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Asking myself the important question in life
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# And testing it
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# And checking it just to be sure
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# And make a comment about it
+print("Oh, that's why it's False")
+
+# I get frustrated because I see there is more on the second page
+print("How about some more")
+
+# But proceed to type it out merrily
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
