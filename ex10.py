d88aeebedb7f296ed39d2d9bd24dd8734bf8fb3f
David Gnamuš <38346533+dgnamus@users.noreply.github.com>
2022-06-09 07:29:44 +0200

    More practice

---
+++ ex10.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+experimental_cat = "I am \vsomething \velse"
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(experimental_cat)
---
+++ ex11.py
@@ -0,0 +1,15 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} kg heavy.")
+
+print("More interesting stuff coming up")
+name = input('What is your name? ')
+love = input('And whom do you love? ')
+do = input('And what do you want to do? ')
+
+print(f"{name}, {love}, {do}")
